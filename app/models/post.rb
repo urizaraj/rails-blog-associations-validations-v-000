e6a801cecf3d757488aaf9fb5abba28024844e6c
Alexander Urizar <urizaraj@gmail.com>
2018-03-13 20:14:35 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, through: :post_tags
+  belongs_to :user
 
   validates :name, presence: true
   validates :content, presence: true
